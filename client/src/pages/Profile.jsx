2e7d5fe8a17a7d3a0c1b53b07aae92f0617af290
spanzy teo <spanzytheo@gmail.com>
2024-06-13 23:05:29 +0100

    complete update listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -273,7 +273,9 @@ const Profile = () => {
                 >
                   Delete
                 </button>
+                <Link to={`/update-listing/${listing._id}`}>
                 <button className="text-green-700 uppercase ">Edit</button>
+                </Link>
               </div>
               <p className="text-red-700">
                 {deleteError ? 'Error deleting your listing' : ''}
