3db42954fa06afa604b67c6b1ae6765ea3b38994
spanzy teo <spanzytheo@gmail.com>
2024-06-16 07:56:32 +0100

    Create the listing item component and show listings

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import ListingItem from '../components/ListingItem'
 
 const Search = () => {
   const navigate = useNavigate()
@@ -14,7 +15,7 @@ const Search = () => {
   })
   const [loading, setLoading] = useState(false)
   const [listings, setListings] = useState([])
-  console.log(listings);
+  console.log(listings)
 
   const handleChange = (e) => {
     if (
@@ -77,16 +78,16 @@ const Search = () => {
     }
 
     const fetchListings = async () => {
-        setLoading(true)
-        const searchQuery = urlParams.toString()
-        const res = await fetch(`/api/listing/get?${searchQuery}`)
-        const data = await res.json()
-        setListings(data)
-        setLoading(false)
+      setLoading(true)
+      const searchQuery = urlParams.toString()
+      const res = await fetch(`/api/listing/get?${searchQuery}`)
+      const data = await res.json()
+      setListings(data)
+      setLoading(false)
     }
 
     fetchListings()
-  }, [location.search])  
+  }, [location.search])
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -203,10 +204,25 @@ const Search = () => {
           </button>
         </form>
       </div>
-      <div className="">
+      <div className="flex-1">
         <h1 className="text-3xl font-semibold border-b p-3 text-slate-700 mt-5">
           Listing Results:
         </h1>
+        <div className="p-7 flex flex-wrap gap-4">
+          {!loading && listings.length === 0 && (
+            <p className="text-xl text-slate-700">No Listing found!</p>
+          )}
+          {loading && (
+            <p className="text-xl text-slate-700 text-center w-full">
+              Loading...
+            </p>
+          )}
+          {!loading &&
+            listings &&
+            listings.map((listing) => (
+                <ListingItem key={listing._id} listing={listing} />
+            ))}
+        </div>
       </div>
     </div>
   )
